17704ba07eddd4135d4897a260de72f54f3f6d73
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-19 22:14:12 +0500

    first

--- my_project_part_1/retrieve_view/views.py
+++ my_project_part_1/retrieve_view/views.py
@@ -1,6 +1,11 @@
 from rest_framework import generics
 
+from retrieve_view.serializers import StoreSerializer
+
+from retrieve_view.models import Store
+
 
 # TODO опишите RetrieveView ниже
 class StoreRetrieveView(generics.RetrieveAPIView):
-    pass
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
