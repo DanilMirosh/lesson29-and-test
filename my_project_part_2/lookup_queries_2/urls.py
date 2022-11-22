1df82c6ee87d28b81a44ccac3cc26272ca492d07
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-22 22:13:27 +0500

    fix lookup_queries_2

--- my_project_part_2/lookup_queries_2/urls.py
+++ my_project_part_2/lookup_queries_2/urls.py
@@ -1,5 +1,12 @@
 # TODO здесь необходимо настроить urls приложения
+from django.urls import include, path
+from rest_framework import routers
+
+from lookup_queries_2 import views
+
+simple_router = routers.SimpleRouter()
+simple_router.register('lookup_queries_2', views.StoreViewSet)
 
 urlpatterns = [
-
+    path('', include(simple_router.urls))
 ]
--- my_project_part_2/lookup_queries_2/views.py
+++ my_project_part_2/lookup_queries_2/views.py
@@ -1,5 +1,12 @@
 # TODO опишите view-функцию ниже
+from faker.providers import address
+from rest_framework import viewsets
+
+from lookup_queries_2.models import Store
+
+from lookup_queries_2.serializers import StoreSerializer
 
 
-class StoreView:
-    pass
+class StoreViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.filter(address__endswith="д. 30")
+    serializer_class = StoreSerializer
