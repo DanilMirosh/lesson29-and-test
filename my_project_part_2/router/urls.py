fe1ee2abc480e2ab1c1b70e35aff5629f0191495
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-22 21:52:36 +0500

    fix router

--- my_project_part_2/router/urls.py
+++ my_project_part_2/router/urls.py
@@ -1,5 +1,12 @@
 # TODO опишите здесь маршрутизацию с помощью класса SimpleRouter
+from django.urls import path, include
+from rest_framework import routers
+
+from router import views
+
+router = routers.SimpleRouter()
+router.register('router_stores', views.StoreViewSet)
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
