d7920ff70ddbca7a75e4ec6701a25005063a9fdc
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-22 23:08:58 +0500

    fix delete_if_null

--- my_project_part_2/delete_if_null/urls.py
+++ my_project_part_2/delete_if_null/urls.py
@@ -1,5 +1,8 @@
 # TODO здесь необходимо настроить urls приложения
+from django.urls import path
+
+from delete_if_null import views
 
 urlpatterns = [
-
+    path('delete_if_null/', views.StoreListView.as_view())
 ]
--- my_project_part_2/delete_if_null/views.py
+++ my_project_part_2/delete_if_null/views.py
@@ -1,5 +1,15 @@
 # TODO опишите view-функцию ниже
+from rest_framework import generics
+
+from delete_if_null.models import Store
+from delete_if_null.serializers import StoreSerializer
 
 
-class StoreView:
-    pass
+class StoreListView(generics.ListAPIView):
+    serializer_class = StoreSerializer
+    queryset = Store.objects.all()
+
+    def get_queryset(self):
+        Store.objects.filter(visits=0).delete()
+
+        return super().get_queryset()
