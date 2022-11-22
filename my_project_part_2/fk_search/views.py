822b3557747d3756e64a43f58a8e5d02e86824fc
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-22 23:01:00 +0500

    fix fk_search

--- my_project_part_2/fk_search/views.py
+++ my_project_part_2/fk_search/views.py
@@ -1,5 +1,9 @@
 # TODO опишите view-функцию ниже
+from rest_framework import viewsets
+from fk_search.models import Store
+from fk_search.serializers import StoreSerializer
 
 
-class StoreView:
-    pass
+class StoreViewSet(viewsets.ModelViewSet):
+    serializer_class = StoreSerializer
+    queryset = Store.objects.filter(city__name='Самара')
