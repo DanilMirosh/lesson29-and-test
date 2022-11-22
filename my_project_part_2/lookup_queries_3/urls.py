29fd06c251561332db2010296f459f9c9d6063dc
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-22 22:25:13 +0500

    fix lookup_queries_3

--- my_project_part_2/lookup_queries_3/urls.py
+++ my_project_part_2/lookup_queries_3/urls.py
@@ -1,5 +1,12 @@
 # TODO здесь необходимо настроить urls приложения
+from rest_framework import routers
+from django.urls import path, include
+
+from lookup_queries_3 import views
+
+simple_router = routers.SimpleRouter()
+simple_router.register('lookup_queries_3', views.StoreViewSet)
 
 urlpatterns = [
-
+   path('', include(simple_router.urls))
 ]
--- my_project_part_2/lookup_queries_3/views.py
+++ my_project_part_2/lookup_queries_3/views.py
@@ -1,5 +1,10 @@
 # TODO опишите view-функцию ниже
+from rest_framework import viewsets
+
+from lookup_queries_3.serializers import StoreSerializer
+from lookup_queries_3.models import Store
 
 
-class StoreView:
-    pass
+class StoreViewSet(viewsets.ModelViewSet):
+    serializer_class = StoreSerializer
+    queryset = Store.objects.filter(open_hour__range=[8, 10])
