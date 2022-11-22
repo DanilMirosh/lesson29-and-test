09e6c3e180a2ab0335ec80948dc9b749db261908
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-22 22:50:07 +0500

    fix qf_queries

--- my_project_part_2/qf_queries/urls.py
+++ my_project_part_2/qf_queries/urls.py
@@ -1,5 +1,12 @@
 # TODO здесь необходимо настроить urls приложения
+from rest_framework import routers
+from django.urls import path, include
+
+from qf_queries import views
+
+simple_router = routers.SimpleRouter()
+simple_router.register('qf_queries', views.StoreViewSet)
 
 urlpatterns = [
-
+   path('', include(simple_router.urls))
 ]
--- my_project_part_2/qf_queries/views.py
+++ my_project_part_2/qf_queries/views.py
@@ -1,5 +1,14 @@
 # TODO опишите view-функцию ниже
+from django.db.models import Q
+from rest_framework import viewsets
+
+from qf_queries.models import Store
+from qf_queries.serializers import StoreSerializer
 
 
-class StoreView:
-    pass
+class StoreViewSet(viewsets.ModelViewSet):
+    serializer_class = StoreSerializer
+    queryset = Store.objects.filter(
+             Q(email__icontains="ivan") |
+             Q(director__icontains="ivan")
+    )
