418c531faa436c30ee4edcf68422449463a8ac05
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-22 22:03:37 +0500

    fix lookup_queries

--- my_project_part_2/lookup_queries/views.py
+++ my_project_part_2/lookup_queries/views.py
@@ -1,5 +1,13 @@
 # TODO опишите view-функцию ниже
+from rest_framework import viewsets
+
+from lookup_queries.models import Store
+
+from lookup_queries.serializers import StoreSerializer
 
 
-class StoreView:
-    pass
+class StoreViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.filter(name__icontains="пят")
+    serializer_class = StoreSerializer
+
+
